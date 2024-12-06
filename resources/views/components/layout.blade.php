ce1602dce8e9dc31f0f3175adaa0f5adb9966f99
Marius_GBK <mariusbiaou75@gmail.comgit config --global user.email mariusbiaou75@linux.org>
2024-12-07 00:34:50 +0100

    Blade and Tailwind Techniques for  Laravel Views

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@400;500;600&display=swap"
+        rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body class="text-white bg-black">
+<body class="text-white bg-black font-hanken-Grotesk">
     <div class="px-10">
         <nav class="flex justify-between border-b item-center border-white/10">
             <div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,17 @@
 <x-layout>
     <div class="space-y-10">
+
+        <section class="text-center pt-6">
+            <h1 class="text-4xl font-bold">Let's Find Your Next Job</h1>
+
+            <form action="" class="mt-6">
+                <input type="text" placeholder="Web Developer..." class="w-full max-w-xl px-5 py-4 rounded-xl bg-white/25 border-white/10">
+            </form>
+        </section>
         <section>
             <x-section-heading>Featured Jobs</x-section-heading>
 
-            <div class="grid grid-cols-3 gap-8 mt-6">
+            <div class="grid gap-8 mt-6 lg:grid-cols-3">
                  <x-job-card></x-job-card>
                  <x-job-card></x-job-card>
                  <x-job-card></x-job-card>
@@ -12,7 +20,7 @@
           <section>
               <x-section-heading>Tag</x-section-heading>
 
-              <div class="mt-6 space-x-1">
+              <div class="mt-6 space-x-3">
                 <x-tag>Tag</x-tag>
                 <x-tag>Tag</x-tag>
                 <x-tag>Tag</x-tag>
@@ -27,6 +35,12 @@
           <section>
               <x-section-heading>Recents Jobs</x-section-heading>
 
+              <div class="mt-6 space-y-6">
+                    <x-job-card-wide></x-job-card-wide>
+                    <x-job-card-wide></x-job-card-wide>
+                    <x-job-card-wide></x-job-card-wide>
+              </div>
           </section>
+
     </div>
 </x-layout>
