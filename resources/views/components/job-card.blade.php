a451c67ab07a2a43cc56c502e6a81e44a5e6bd49
Marius_GBK <mariusbiaou75@gmail.comgit config --global user.email mariusbiaou75@linux.org>
2024-12-06 12:55:15 +0100

    first commint

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,15 @@
+<div class="flex flex-col p-4 text-center bg-white/5 rounded-xl ">
+    <div class="self-start text-sm">Laracast</div>
+    <div class="py-8 font-bold">
+        <h3>Vidéo Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="flex items-center justify-between mt-auto">
+        <div>
+           <x-tag>Tag</x-tag>
+           <x-tag>Tag</x-tag>
+           <x-tag>Tag</x-tag>
+        </div>
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    </div>
+</div>
